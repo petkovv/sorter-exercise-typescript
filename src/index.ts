dc533752b3118cdfc492914e0b894d28d6c2174a
Plamen Petkov <>
2023-03-19 16:09:14 +0200

    final look of this excercise project. The Linked list is kind of dummy implementation because of not much accurate swap logic implementation. The idea here is to practice TypeScript, not the logic of different data structures.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { NumberCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
-import { Sorter } from "./Sorter";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumberCollection([4, 1, 5]);
-const numbersSorter = new Sorter(numbersCollection);
-numbersSorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data)
 
 const charactersCollection = new CharactersCollection('Ivan');
-const charactersSorter = new Sorter(charactersCollection);
-charactersSorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data)
+
+const linkedList = new LinkedList();
+linkedList.add(500)
+linkedList.add(50)
+linkedList.add(10)
+linkedList.add(200)
+linkedList.add(-500)
+linkedList.add(0)
+linkedList.sort();
+linkedList.print();
